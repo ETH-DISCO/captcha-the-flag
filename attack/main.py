fa32d9cfea5cb32bc09e820ed25472e903e807f1
sueszli <jabaryyahya@gmail.com>
2024-07-25 11:58:44 +0200

    up

--- attack/main.py
+++ attack/main.py
@@ -1,1 +1,11 @@
 # install playwright, find vpn rotation tools
+
+from fastapi import FastAPI
+
+print("runnning main.py")
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
